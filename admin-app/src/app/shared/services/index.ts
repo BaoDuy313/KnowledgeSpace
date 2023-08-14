e5e795c91fab3d6cd8363297f42ebbeb61b32b9a
duyrua <106798993+BaoDuy313@users.noreply.github.com>
2023-08-14 13:52:36 +0700

    Load dynamic menu from backend API

--- admin-app/src/app/shared/services/index.ts
+++ admin-app/src/app/shared/services/index.ts
@@ -1,3 +1,5 @@
-export * from './base.service';
 export * from './auth.service';
-export * from './users.services'
+export * from './base.service';
+export * from './users.services';
+export * from './utilities.service';
+export * from './functions.service';
